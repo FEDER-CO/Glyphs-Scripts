20a080a2867b3eb50271eb64cdb2fb890c322e7c
Federico Parra <federicoparra@gmail.com>
2020-04-13 02:34:36 +0200

    Create Glyphs Masters Overview.py

---
+++ Glyphs Masters Overview.py
@@ -0,0 +1,13 @@
+#MenuTitle: Glyphs Masters Overview
+# -*- coding: utf-8 -*-
+__doc__="""
+Creates a new tab with rows of selected glyphs in all available masters. It ignores brace, bracket or a smart component layers. Useful for comparing the different weights of a typeface.
+"""
+
+font = Glyphs.font
+font.newTab()
+for glyph in font.selection:
+	font.currentTab.layers.append(GSControlLayer(10))
+	for layer in glyph.layers:
+		if layer.isSpecialLayer is False :
+			font.currentTab.layers.append(layer)
