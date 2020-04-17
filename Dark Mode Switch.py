3b067aa07e6bb5fbc0bf364e878dbde077b2208b
Federico Parra <federicoparra@gmail.com>
2020-04-17 11:52:51 +0200

    Create Dark Mode Switch

---
+++ Dark Mode Switch.py
@@ -0,0 +1,7 @@
+#MenuTitle: Glyphs Masters Overview
+# -*- coding: utf-8 -*-
+__doc__="""
+Switches between normal and dark mode.
+"""
+
+Glyphs.boolDefaults["GSEditViewDarkMode"] = not Glyphs.boolDefaults["GSEditViewDarkMode"]
