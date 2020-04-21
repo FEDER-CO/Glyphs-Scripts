02f7d58c45c701e151e39b3e93619e9b472977b4
Federico Parra <federicoparra@gmail.com>
2020-04-21 19:13:10 +0200

    Create Lowercase Control Characters

---
+++ Lowercase Control Characters.py
@@ -0,0 +1,13 @@
+#MenuTitle: Lowercase control characters
+# -*- coding: utf-8 -*-
+__doc__="""
+Creates a new tab with selected glyphs between lowercase control characters "n" and "o".
+"""
+
+font = Glyphs.font
+tab = ''
+for glyph in font.selection:
+    tab += "/n/n/%s/n/o/%s/o/o\n" % (glyph.name, glyph.name)
+
+# open new tab with text
+font.newTab(tab)
