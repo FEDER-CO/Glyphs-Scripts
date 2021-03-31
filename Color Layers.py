d71a73bff97bf99a2289329468bd17dbc60ad536
Federico Parra <federicoparra@gmail.com>
2021-03-30 19:03:08 -0500

    Updats for G3

---
+++ Color Layers.py
@@ -0,0 +1,30 @@
+#MenuTitle: Color Layers
+# -*- coding: utf-8 -*-
+__doc__="""
+Colors glyph layers to differentiate if they are Base Glyphs, Composites or Mixed Composites.
+"""
+
+Font = Glyphs.font
+baseGlyphs = []
+composites = []
+mixedComposites = []
+
+for glyph in Font.glyphs:
+	for layer in glyph.layers:
+		componentCount = len(layer.components)
+		pathsCount = len(layer.paths)	
+		if componentCount == 0:
+			baseGlyphs.append(layer)
+		else:
+			if pathsCount == 0:
+				composites.append(layer)
+			else:
+				mixedComposites.append(layer)			
+						
+for layer in baseGlyphs:
+	layer.setColorIndex_(6)
+for layer in composites:
+	layer.setColorIndex_(10)
+for layer in mixedComposites:
+	layer.setColorIndex_(3)
+	
